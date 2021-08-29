40ecc6c765b93a7cba81e39be4eb7f9fa68c2777
Ghiri <ghirihari@gmail.com>
2021-08-29 23:43:19 +0530

    Reviews

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -8,6 +8,10 @@ const MainScreen = (props) => {
         "https://prod-img.thesouledstore.com/public/theSoul/uploads/slider/20210829115515.jpg?format=webp&w=1242&dpr=1.1",
         "https://prod-img.thesouledstore.com/public/theSoul/uploads/slider/20210822105650.jpg?format=webp&w=1242&dpr=1.1",        
     ]
+    const reviews = [
+        {name:'Ghiri Hari Vvz', rating:'⭐⭐⭐⭐⭐',review:"The Return of the King is by far the most moving, inspirational, and epic of Peter Jackson's The Lord of the Rings Trilogy movies. The movie plot is compelling with themes of friendship, strength, and loyalty. Frodo Baggins and Samwise Gamgee continue their quest to destroy the One Ring at Mount Doom led by Gollum. Gollum becomes more cunning and sneaky than ever. Gollum becomes more cunning and sneaky than ever. Gandalf the White, Aragorn, Legolas and Gimli regroup with Merry and Pippin in Isengard, and they learn that Sauron is planning a full-scale attack on the largest city of men, Minas Tirith in Gondor.",imgUrl:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1626345131_8042057.jpg?format=webp&w=376&dpr=1.0"}
+    ]
+
 
     const navigate = (direction) => {
         switch(direction){
@@ -49,7 +53,27 @@ const MainScreen = (props) => {
                     }
                 </div>
             </div>
-      </div>
+            <div className="reviewWrapper">
+                <div className="title">- Recent Reviews -</div>
+                <div className="reviewContainer">
+                    <div className="review">                    
+                        <div>{reviews[0].name}</div>
+                        <div>{reviews[0].rating}</div>
+                        <div className="reviewText">{reviews[0].review}</div>
+                    </div>
+                    <div className="review">
+                        <div>{reviews[0].name}</div>
+                        <div>{reviews[0].rating}</div>
+                        <div className="reviewText">{reviews[0].review}</div>
+                    </div>
+                    <div className="review">
+                        <div>{reviews[0].name}</div>
+                        <div>{reviews[0].rating}</div>
+                        <div className="reviewText">{reviews[0].review}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
